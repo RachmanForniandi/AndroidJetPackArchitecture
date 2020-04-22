3231091e959d929cea84d09339938820d0d4c907
RachmanForniandi <rachmanforniandi@gmail.com>
2020-04-23 00:16:41 +0700

    add nav host fragment & nav destination fragment done

---
+++ Navigation_arch_component/NavDemo1/app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.anushka.navdemo1.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        return fragmentHomeBinding.root
+    }
+
+}
---
+++ Navigation_arch_component/NavDemo1/app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -0,0 +1,26 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.anushka.navdemo1.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+    private lateinit var secondBinding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        secondBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+        return secondBinding.root
+    }
+
+}
