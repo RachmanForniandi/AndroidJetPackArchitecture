9269883c2e0f949f3335cea782180293c4cb3761
RachmanForniandi <rachmanforniandi@gmail.com>
2020-05-28 22:51:42 +0700

    added sample tap action notification done

--- Notification/LearnNotification/app/src/main/java/com/example/learnnotification/MainActivity.kt
+++ Notification/LearnNotification/app/src/main/java/com/example/learnnotification/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.learnnotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -25,12 +27,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification() {
         val idNotification = 45
+        val tapResultIntent = Intent(this,SecondActivity::class.java)
+        val pendingIntent:PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
         val notification = NotificationCompat.Builder(this@MainActivity,channelId)
             .setContentTitle("Demo Title Notification")
             .setContentText("This is a demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(idNotification,notification)
 
