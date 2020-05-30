b228ce379fa793a216a20d7ea3ad71d536624b0c
RachmanForniandi <rachmanforniandi@gmail.com>
2020-05-30 14:05:22 +0700

    added sample work manager one time request done

---
+++ Work_Manager_new/LearnWorkManagerNewApp/app/src/main/java/com/example/learnworkmanagernewapp/UploadWorker.kt
@@ -0,0 +1,19 @@
+package com.example.learnworkmanagernewapp
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(context: Context, params:WorkerParameters): Worker(context,params) {
+    override fun doWork(): Result {
+        try {
+            for (i in 0..600){
+                Log.i("MYTAG","Uploading $i")
+            }
+            return Result.success()
+        }catch (e:Exception){
+            return Result.failure()
+        }
+    }
+}
