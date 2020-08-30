9fde8eb13863e541f4edee090a22b9da0c912f7f
RachmanForniandi <rachmanforniandi@gmail.com>
2020-08-30 11:09:25 +0700

    simple example of injection

--- Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*val battery = Battery()
+        val memoryCard = MemoryCard()
+        val serviceProvider = ServiceProvider()
+        val simCard = SIMCard(serviceProvider)*/
+        val smartPhone = SmartPhone(Battery(), SIMCard(ServiceProvider()), MemoryCard())
+
+        smartPhone.makeACallWithRecording()
     }
 }
--- Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/SIMCard.kt
+++ Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/SIMCard.kt
@@ -3,9 +3,16 @@ package com.anushka.didemo
 import android.util.Log
 
 class SIMCard(private  val serviceProvider: ServiceProvider) {
+
+
+    //private lateinit var serviceProvider: ServiceProvider
     init {
         Log.i("MYTAG","SIM Card Constructed")
     }
+
+    /*fun setServiceProvider(serviceProvider: ServiceProvider){
+        this.serviceProvider = serviceProvider
+    }*/
 
     fun getConnection(){
         serviceProvider.getServiceProvider()
