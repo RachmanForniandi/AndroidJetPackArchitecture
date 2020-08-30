c59a7ed57fb934dcd87a568cf074ed9b16282d70
RachmanForniandi <rachmanforniandi@gmail.com>
2020-08-30 16:24:15 +0700

    field injection by dagger done.

--- Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ Depedency_Injection_by_Dagger_2/DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -2,18 +2,20 @@ package com.anushka.didemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         DaggerSmartPhoneComponent.create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+            //.getSmartPhone()
+            .inject(this)
+            smartPhone.makeACallWithRecording()
 
         
 
