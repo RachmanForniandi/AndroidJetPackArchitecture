cab90c1206e51f74c5c520f01885c042f42cd5f1
RachmanForniandi <rachmanforniandi@gmail.com>
2021-04-17 16:13:47 +0700

    implement viewbinding done

--- viewBinding/BindingDemo1/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
+++ viewBinding/BindingDemo1/app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
@@ -7,9 +7,12 @@ import com.anushka.bindingdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        /*binding = DataBindingUtil.setContentView(this, R.layout.activity_main)*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.submitButton.setOnClickListener {
             displayGreeting()
